37319c9e9b1f52b083bd293947ddf9f720ffe4f1
Arpanbhagat5 <arpanbhagat5@gmail.com>
2021-11-10 03:20:06 +0900

    Implement various methods for tables, orders entities

--- src/orders.rs
+++ src/orders.rs
@@ -1,85 +1,170 @@
-// use diesel;
-// use diesel::prelude::*;
-// use diesel::pg::PgConnection;
+use diesel;
+use diesel::dsl::*;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use diesel::sql_query;
+use diesel::sql_types::*;
+
+use super::schema::tables;
+use super::schema::status;
+use super::schema::menu;
+use super::schema::orders;
+use super::schema::order_items;
+
+use crate::tables::Tables;
+use crate::orders::orders::dsl::orders as all_orders;
 
 
-// use super::schema::tables;
-// use super::schema::status;
+// This was a saver at 2AM
+#[derive(QueryableByName, Debug)]
+struct IntOrder {
+    #[sql_type = "Integer"]
+    order_id: i32
+}
 
-// // use super::schema::orders;
-// use super::schema::orders::dsl::orders as orders;
+// Used when SQL query needs a single value
+#[derive(QueryableByName, Debug)]
+struct IntTable {
+    #[sql_type = "Integer"]
+    table_id: i32
+}
 
-// // use super::schema::order_items;
-// use super::schema::order_items::dsl::order_items as order_items;
+#[derive(Identifiable, Queryable, Insertable)]
+#[primary_key(order_id)]
+#[table_name = "orders"]
+pub struct Orders {
+  pub order_id: i32,
+  pub table_id: i32,
+}
 
+#[derive(Queryable, Insertable, Debug)]
+#[table_name = "orders"]
+pub struct NewOrder {
+  pub table_id: i32,
+}
 
-// #[derive(Insertable, Identifiable, Queryable, Associations)]
-// #[belongs_to(Table, foreign_key="table_id")]
-// #[primary_key(order_id)]
-// #[table_name = "orders"]
-// pub struct Orders {
-//   pub order_id: i32,
-//   pub table_id: i32,
-// }
+#[derive(Identifiable, Queryable, QueryableByName, Debug)]
+#[primary_key(id)]
+#[table_name = "order_items"]
+pub struct OrderItems {
+  pub id: i32,
+  pub prep_time: i32,
+  pub order_id: i32,
+  pub item_id: i32,
+  pub status_id: i32,
+}
 
+#[derive(QueryableByName, Debug)]
+struct IntCount {
+    #[sql_type = "BigInt"]
+    count: i64
+}
 
-// #[derive(Queryable, Insertable, Debug)]
-// #[table_name = "orders"]
-// pub struct NewOrder {
-//   pub table_id: i32,
-// }
+#[derive(Insertable, Queryable, Debug)]
+#[table_name = "order_items"]
+pub struct NewOrderItem {
+  pub order_id: i32,
+  pub item_id: i32,
+  pub prep_time: i32,
+  pub status_id: i32,
+}
 
+impl Orders {
+  pub fn create_order(new_order: NewOrder, table_id: i32, conn: &PgConnection) -> bool {
+    let is_occupied = Tables::is_occupied(new_order.table_id, &conn);
 
-// #[derive(Identifiable, Queryable, Associations)]
-// #[belongs_to(Orders, foreign_key="order_id")]
-// #[belongs_to(Menu, foreign_key="item_id")]
-// #[belongs_to(Status, foreign_key="status_id")]
-// #[primary_key(id)]
-// pub struct OrderItems {
-//   pub id: i32,
-//   pub prep_time: i32,
-//   pub order_id: i32,
-//   pub item_id: i32,
-//   pub status_id: i32,
+    // TODO: handle the case where 2nd query fails, maybe need retry mechanism
+    if !is_occupied {
+      let order_created = diesel::insert_into(orders::table)
+      .values(new_order)
+      .execute(conn)
+      .is_ok();
 
-// }
+      let table_occupied = Tables::set_occupied(table_id, &conn);
+      return order_created && table_occupied
+    }
+    else {
+      println!("Table is already occupied!");
+      return false
+    }
+  }
 
-// pub struct NewOrderItemsRow {
-//   pub order_id: i32,
-//   pub item_id: i32,
-//   pub prep_time: i32,
-//   pub status_id: i32,
-// }
+  pub fn add_item_to_order(order_id: i32, new_order_item: NewOrderItem, conn: &PgConnection) -> bool {
+    println!("{:?}", new_order_item); //How come the obj is still accessible in diesel query below?
+    diesel::insert_into(order_items::table)
+      .values(new_order_item)
+      .execute(conn)
+      .is_ok()
+  }
 
-// impl Orders{
-//   pub fn create_order(new_order: NewOrder, conn: &PgConnection) -> bool {
-//     diesel::insert_into(orders)
-//       .values(new_order)
-//       .execute(conn)
-//       .is_ok()
-//   }
+  pub fn get_order_id_from_table_id(table_id: i32, conn: &PgConnection) -> i32 {
+    let result:Vec<IntOrder> = sql_query("SELECT order_id FROM orders WHERE table_id=$1")
+      .bind::<Integer, _>(table_id)
+      .load(conn)
+      .unwrap();
+    let order_id: i32 = result[0].order_id;
+    return order_id
+  }
 
-//   pub fn add_item_to_order(order_id: i32, new_row: NewOrderItemsRow, conn: &PgConnection) -> bool {
-//     diesel::insert_into(order_items)
-//       .values(new_row)
-//       .execute(conn)
-//       .is_ok()
-//   }
+  pub fn are_all_orders_served(table_id: i32, conn: &PgConnection) -> bool {
+    // TODO: Handle incorrect table_id
+    // Get order_id from table_id
+    let order_id = Orders::get_order_id_from_table_id(table_id, &conn);
 
-//   fn get_order_items_row_id_from_item_name(order_id: i32, item_name: String) -> Vec<OrderItems> {
-//     order_items
-//       .filter(order_items::order_id.eq(order_id), order_items::item_name.eq(item_name))
-//       .load::<OrderItems>(&conn)
-//       .expect("No entry for such item name for specified order_id")
-//   }
+    // Get count of orders not "served"
+    let result:Vec<IntCount> = sql_query("SELECT COUNT(order_id) FROM order_items WHERE order_id=$1 AND status_id=5") //TODO status!=8
+      .bind::<Integer, _>(order_id)
+      .load(conn)
+      .unwrap();
+    let count = result[0].count;
 
-//   pub fn delete_item_from_order(order_id: i32, item_name: String, conn: &PgConnection) -> bool {
-//     order_row = self.get_order_items_row_id_from_item_name(order_id, item_name);
-//     diesel::delete(order_items.find(order_row.id))
-//       .execute(&conn)
-//       .is_ok()
-//   }
+    // if count of not "served" return false
+    if count > 0 {
+      return false
+    }
+    else {
+      return true
+    }
+  }
 
-//   // pub fn get_order_items_by_table(table_id: i32, connection: &PgConnection) -> Vec(OrderItems) {
-//   // }
-// }
+  // pub fn get_full_order_list_for_table(table_id: i32, conn: &PgConnection) -> Vec<OrderItems> {
+  //   let order_num = Orders::get_order_id_from_table_id(table_id, &conn);
+
+  //   let result: Vec<OrderItems> = sql_query("SELECT * FROM order_items WHERE order_id=$1")
+  //     .bind::<Integer, _>(order_id)
+  //     .get_results(conn)
+  //     .unwrap();
+  //   println!("{:?}", result);
+
+    // use self::order_items::dsl::*;
+    // let result: Vec<OrderItems> = order_items
+    //   .filter(order_id.eq(&order_num))
+    //   .load(conn);
+    // return result
+  // }
+
+  // pub fn get_order_list_for_table_by_status(table_id: i32, status_id: i32, conn: &PgConnection) -> Vec<OrderItems> {
+  //   let order_id = Orders::get_order_id_from_table_id(table_id, &conn);
+  //   let result:Vec<OrderItems> = sql_query("SELECT * FROM order_items WHERE order_id=$1 AND status_id=$2") // status!=8
+  //     .bind::<Integer, _>(order_id)
+  //     .bind::<Integer, _>(status_id)
+  //     .load(conn)
+  //     .unwrap();
+  // }
+
+    // fn get_order_items_row_id_from_item_name(order_id: i32, item_id: i32, conn: &PgConnection) -> Vec<OrderItems> {
+  //   order_items::table
+  //     .filter((order_id.eq(&order_id), item_id.eq(&item_id)))
+  //     .load::<OrderItems>(conn)
+  //     .expect("No entry for such item name for specified order_id")
+  // }
+
+  // pub fn delete_item_from_order(order_id: i32, item: String, conn: &PgConnection) -> bool {
+
+  //   let order_row = Orders::get_order_items_row_id_from_item_name(order_id, 1);
+  //   diesel::delete(order_items::table.find(id.eq(&order_row.id)))
+  //     .execute(conn)
+  //     .is_ok()
+  // }
+
+}
--- src/tables.rs
+++ src/tables.rs
@@ -1,14 +1,22 @@
 use diesel;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
+use diesel::result::Error;
+
+use diesel::types::ToSql;
+use diesel::sql_types::Bool;
+
+use crate::orders::Orders;
 
 use super::schema::tables;
+use super::schema::tables::dsl::tables as tables_items;
 
 
 #[derive(Queryable, Debug)]
 pub struct Tables {
   pub table_id: i32,
   pub table_name: String,
+  pub is_occupied: bool,
 }
 
 
@@ -16,6 +24,7 @@ pub struct Tables {
 #[table_name = "tables"]
 pub struct NewTable {
   pub table_name: String,
+  pub is_occupied: bool,
 }
 
 impl Tables {
@@ -24,5 +33,37 @@ impl Tables {
       .values(new_table)
       .execute(conn)
       .is_ok()
+  }
+
+  pub fn is_occupied(table_id: i32, conn: &PgConnection ) -> bool {
+    tables_items
+      .find(table_id)
+      .select(self::tables::is_occupied)
+      .get_result(conn)
+      .expect("Could not get table with specified id!")
+  }
+
+  pub fn set_occupied(table_id: i32, conn: &PgConnection ) -> bool {
+    diesel::update(tables_items.find(table_id))
+      .set(self::tables::is_occupied.eq(true))
+      .get_result::<Tables>(conn)
+      .is_ok()
+  }
+
+  pub fn set_table_free(table_id: i32, conn: &PgConnection ) {
+    // Check if all orders are "served"
+    let all_orders_served = Orders::are_all_orders_served(10, &conn);
+
+    // Set table free by setting is_occupied flag as false
+    if all_orders_served {
+      diesel::update(tables_items.find(table_id))
+        .set(self::tables::is_occupied.eq(false))
+        .get_result::<Tables>(conn)
+        .is_ok();
+      println!("Table is free now");
+    }
+    else {
+      println!("Cannot set free: Table has unserved items");
+    }
   }
 }
